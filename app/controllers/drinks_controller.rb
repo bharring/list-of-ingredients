6dc116e84536a1937608014ec27637f54b9bcf46
Bradley Harrington <bharring@gmail.com>
2017-11-03 11:24:57 -0500

    mvp

--- app/controllers/drinks_controller.rb
+++ app/controllers/drinks_controller.rb
@@ -1,4 +1,4 @@
-class DrinksController < ApplicationController
+class DrinksController < ApiController
   before_action :set_drink, only: [:show, :update, :destroy]
 
   # GET /drinks
@@ -11,31 +11,6 @@ class DrinksController < ApplicationController
   # GET /drinks/1
   def show
     render json: @drink
-  end
-
-  # POST /drinks
-  def create
-    @drink = Drink.new(drink_params)
-
-    if @drink.save
-      render json: @drink, status: :created, location: @drink
-    else
-      render json: @drink.errors, status: :unprocessable_entity
-    end
-  end
-
-  # PATCH/PUT /drinks/1
-  def update
-    if @drink.update(drink_params)
-      render json: @drink
-    else
-      render json: @drink.errors, status: :unprocessable_entity
-    end
-  end
-
-  # DELETE /drinks/1
-  def destroy
-    @drink.destroy
   end
 
   private
