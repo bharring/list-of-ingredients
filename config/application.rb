753a653f784b89f4bbeff55a0da12dfea60f86f4
Bradley Harrington <bharring@gmail.com>
2017-11-03 00:29:19 -0500

    show drinks

--- config/application.rb
+++ config/application.rb
@@ -29,6 +29,9 @@ module ListOfIngredients
     # Middleware like session, flash, cookies can be added back manually.
     # Skip views, helpers and assets when generating a new resource.
     config.api_only = true
+    config.generators do |g|
+        g.scaffold_controller = :scaffold_controller
+    end
 
     config.middleware.use Rack::MethodOverride
     config.middleware.use ActionDispatch::Flash
